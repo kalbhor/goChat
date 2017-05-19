ae85fd627c029e983a6be69db6e0b933e3599d6a
Lakshay Kalbhor <lakshaykalbhor@gmail.com>
2017-05-19 20:33:09 +0530

    Formatted message outputs

--- server.go
+++ server.go
@@ -6,14 +6,17 @@ import (
 "os"
 "net"
 "bufio"
+"strings"
 )
 
 func main(){
 
-	userCount := 0
+	userCount := 1
+	maxUsers := 2 // By default
 
-	users := make(map[net.Conn] int) // Map of active connections
-	newUser := make(chan net.Conn) // New connection
+	users := make(map[net.Conn] string) // Map of active connections
+	newUser := make(chan net.Conn) // Handle new connection
+	addedUser := make(chan net.Conn) // Add new connection
 	deadUser := make(chan net.Conn) // Users that have left
 	messages := make(chan string) // channel that recieves messages from all users
 
@@ -23,50 +26,61 @@ func main(){
 		os.Exit(1)
 	}
 
-	go func(){ // Launch thread that will accept connections forever
+	go func(){ // Launch routine that will accept connections forever
 		for{
 			conn, err := server.Accept()
 			if err != nil {
 				fmt.Println(err)
 				os.Exit(1)
 			}
-
-			newUser <- conn
-			messages <- fmt.Sprintf("Accepted new user, #%d\n", userCount)
+			if userCount > maxUsers {
+				os.Exit(1)
+			}
+			newUser <- conn // Send to handle new user
 		}
 	}()
 
 	for{
 
 		select{
+		case conn := <-newUser:
+			go func(conn net.Conn){ // Ask user for name and information
+				reader := bufio.NewReader(conn)
+				conn.Write([]byte("Enter name: "))
+				userName, _ := reader.ReadString('\n')
+				userName = strings.Trim(userName, "\r\n")
+				log.Printf("Accepted new user : %s", userName)
+				messages <- fmt.Sprintf("Accepted user : [%s]\n\n", userName)
 
-		case conn := <-newUser: // If new connection
-			log.Printf("Accepted new user, #%d", userCount)
-			fmt.Print('\a')
+				users[conn] = userName // Add connection
+				userCount++ // Increment the usercount
 
-			users[conn] = userCount // Add connection
-			userCount++ // Increment the usercount
+				addedUser <- conn // Add user to pool
+			}(conn)
 
-			go func(conn net.Conn, userId int) { // launch a thread that handles messages (1 thread per user)
+		case conn := <-addedUser: // Launch a new go routine for the newly added user
+
+			go func(conn net.Conn, userName string) {
 				reader := bufio.NewReader(conn)
 				for {
 					newMessage, err := reader.ReadString('\n')
+					newMessage = strings.Trim(newMessage, "\r\n")
 					if err != nil{
 						break
 					}
-					messages <- fmt.Sprintf("User #%d : %s\a\n", userId, newMessage) // Send to messages channel and ring every user
-
+					// Send to messages channel therefore ring every user
+					messages <- fmt.Sprintf(">%s: %s \a\n", userName, newMessage)
 				}
 
 				deadUser <- conn // If error occurs, connection has been terminated
-				messages <- fmt.Sprintf("Client disconnected\n")
+				messages <- fmt.Sprintf("%s disconnected\n", userName)
 			}(conn, users[conn])
 
 		case message := <- messages: // If message recieved from any user
 
 			for conn, _ := range users { // Send to all users
-				go func(conn net.Conn, message string){
-					_, err := conn.Write([]byte(message)) // Write to all user connections
+				go func(conn net.Conn, message string){ // Write to all user connections
+					_, err := conn.Write([]byte(message))
 
 					if err != nil{
 						deadUser <- conn
@@ -77,9 +91,9 @@ func main(){
 			}
 
 		case conn := <- deadUser: // Handle dead users
-			// messages <- fmt.Sprintf("Client disconnected\n") // Announce that user has left
 			log.Printf("Client disconnected")
 			delete(users, conn)
+			userCount--
 		}
 	}
 }
